6868ad43e05aaac4b1b61e43b054a22839d17982
Adarsh <adarshkhandelwal.work@gmail.com>
2020-01-29 01:14:21 +0530

    change listen route

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/", indexRoutes);
 
-app.listen("8080",function(){
+app.listen(process.env.PORT, process.env.IP,function(){
 	console.log("YelpCamp Server");
 });
